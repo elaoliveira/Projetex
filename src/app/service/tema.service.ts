2581e47b2962d4e56979ca729f4dda9656230167
Rafaela <rafaela.o.barbosa@gmail.com>
2021-11-11 16:00:18 -0300

    [Feature Rafa - Task: Implemented put and delete tema]

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -26,10 +26,20 @@ export class TemaService {
     return this.http.get<Tema[]>('https://projetinblogpessoal.herokuapp.com/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://projetinblogpessoal.herokuapp.com/tema/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://projetinblogpessoal.herokuapp.com/tema', tema, this.token)
   }
 
-  
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://projetinblogpessoal.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://projetinblogpessoal.herokuapp.com/tema/${id}`, this.token)
+  }
   
 }
