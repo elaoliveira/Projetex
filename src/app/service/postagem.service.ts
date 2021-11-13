7395213e725a8b6c1315c0389cf54aec97c962d5
Rafaela <rafaela.o.barbosa@gmail.com>
2021-11-13 15:49:06 -0300

    [Feature Rafa - Task: Implemented put and delete]

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -19,7 +19,19 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://projetinblogpessoal.herokuapp.com/postagem', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://projetinblogpessoal.herokuapp.com/postagem/${id}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://projetinblogpessoal.herokuapp.com/postagem', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://projetinblogpessoal.herokuapp.com/postagem', postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete(`https://projetinblogpessoal.herokuapp.com/postagem/${id}`, this.token)
   }
 }
